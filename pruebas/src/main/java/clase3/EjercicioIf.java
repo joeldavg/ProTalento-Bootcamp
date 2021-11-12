a0571beaeef7f23fa5cb3688be3cc017dd1a1f48
joeldavg <joel-guzman@hotmail.com>
2021-11-11 23:34:59 -0500

    EjercicioIf modificado ---> OK

--- pruebas/src/main/java/clase3/EjercicioIf.java
+++ pruebas/src/main/java/clase3/EjercicioIf.java
@@ -28,13 +28,13 @@ public class EjercicioIf {
 		n3 = teclado.nextDouble();
 		
 		//
-		if (n1 >= n2 && n1 > n3) {
+		if (n1 >= n2 && n1 >= n3) {
 			System.out.println("El numero mayor ingresado es: " + n1);
 		}
-		else if (n2 >= n1 && n2 > n3) {
+		else if (n2 >= n1 && n2 >= n3) {
 			System.out.println("El numero mayor ingresado es: " + n2);
 		}
-		else if (n3 >= n1 && n3 > n2) {
+		else if (n3 >= n1 && n3 >= n2) {
 			System.out.println("El numero mayor ingresado es: " + n3);
 		}
 		else {
