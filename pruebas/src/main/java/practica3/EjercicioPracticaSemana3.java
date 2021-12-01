43ac38c61098e479b7338aa9563503b498382303
joeldavg <joel-guzman@hotmail.com>
2021-12-01 00:59:28 -0500

    ---ejercicio escuela finalizado - ok

--- pruebas/src/main/java/practica3/EjercicioPracticaSemana3.java
+++ pruebas/src/main/java/practica3/EjercicioPracticaSemana3.java
@@ -3,9 +3,11 @@ package practica3;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import javax.swing.ProgressMonitor;
+
 import org.w3c.dom.Notation;
 
-public class Ejercicio {
+public class EjercicioPracticaSemana3 {
 
 	public static void main(String[] args) {
 		/*
@@ -27,13 +29,12 @@ public class Ejercicio {
 		 
 		 */
 
-
 		Integer cantidadAlumno;
+		Integer numeroDeExamen;
 		String[] nombre;
 		String[] apellido;
-		Integer[] numeroDeExamen;
-		Float[] notaAlumno;
-		Object[][] alumno;
+		Float[][] notaAlumno;
+		//Object[][] alumno;
 		
 		Object[][] alumnoNotaMasAlta;
 		Object[][] alumnoNotaMasBaja;
@@ -45,6 +46,7 @@ public class Ejercicio {
 		Scanner teclado = new Scanner(System.in);
 				
 		System.out.println("Bienvenidos a La escuela primaria xyz");
+		System.out.println();
 		
 		// Ingresar cantidad alumno
 		
@@ -53,36 +55,41 @@ public class Ejercicio {
 			System.out.println("Ingrese cantidad de alumnos a procesar > 0:");
 			cantidadAlumno = teclado.nextInt();
 			
-		} while (cantidadAlumno <= 0);
+			System.out.println("Ingrese cantidad de notas > 0:");
+			numeroDeExamen = teclado.nextInt();
+			
+		} while (cantidadAlumno <= 0 && numeroDeExamen <=0);
 		
 		// definir tamaño 
 		
 		nombre = new String[cantidadAlumno];
 		apellido = new String[cantidadAlumno];
-		numeroDeExamen = new Integer[cantidadAlumno];
-		notaAlumno = new Float[cantidadAlumno];
+		notaAlumno = new Float[cantidadAlumno][numeroDeExamen];
 		
-		
-		alumno = new Object[cantidadAlumno][4];
 		
 		// ingresando valores
 		
 		for (int i = 0; i < cantidadAlumno; i++) {
 			
+			System.out.println();
 			System.out.println("Alumno #" + (i+1) + ", ingresar nombre:");
 			nombre[i] = teclado.next();
 			
+			System.out.println();
 			System.out.println("Alumno #" + (i+1) + ", ingresar apellido:");
 			apellido[i] = teclado.next();
-
-			System.out.println("Alumno #" + (i+1) + ", ingresar numero de examen:");
-			numeroDeExamen[i] = teclado.nextInt();
-						
-			do {
-				System.out.println("Alumno #" + (i+1) + ", ingresar nota (númerico >=0 <=10):");
-				notaAlumno[i] = teclado.nextFloat();
+			
+			for (int j = 0; j < numeroDeExamen; j++) {
 				
-			} while (notaAlumno[i] < 0 || notaAlumno[i] > 10);
+				do {
+					System.out.println();
+					System.out.println("Alumno #" + (i+1) + ", ingresar nota #" + (j+1) + " (númerico >=0 <=10):");
+					notaAlumno[i][j] = teclado.nextFloat();
+					
+				} while (notaAlumno[i][j] < 0 || notaAlumno[i][j] > 10);
+			
+			}
+						
 			
 		}
 		
@@ -90,32 +97,52 @@ public class Ejercicio {
 		
 		// procesamiento de datos
 		
-		Float auxNotaMasAlta = notaAlumno[0];
-		Float auxNotaMasBaja = notaAlumno[0];
+		//calcular promedio de notas
 		
-		int contadorNotaMasAlta = 1;
-		int contadorNotaMasBaja = 1;
+		Float[] promNotaAlumno = new Float[notaAlumno.length];
 		
-		for (int i = 1; i < notaAlumno.length; i++) {
+		for (int i = 0; i < notaAlumno.length; i++) {
 			
-			if ( auxNotaMasAlta <= notaAlumno[i] ) {
-				
-				if ( auxNotaMasAlta.equals(notaAlumno[i]) ) {
-					contadorNotaMasAlta++;
-				} else {
-					contadorNotaMasAlta = 1;
-				}
-				auxNotaMasAlta = notaAlumno[i];
+			Float sumaNota = 0F;
+			
+			for (int j = 0; j < notaAlumno[i].length; j++) {
+				sumaNota += notaAlumno[i][j];
 			}
 			
-			if (auxNotaMasBaja >= notaAlumno[i]) {
+			promNotaAlumno[i] = sumaNota / notaAlumno[i].length;
+		}
+		
+		//alumno = new Object[][] { {nombre}, {apellido}, {notaAlumno}, {promNotaAlumno} };
+		
+		//buscar promedio mas alto y mas bajo
+		
+		Float auxPromMasAlto = promNotaAlumno[0];
+		Float auxPromMasBajo = promNotaAlumno[0];
+		
+		int contadorPromNotaMasAlta = 1;
+		int contadorPromNotaMasBaja = 1;
+		
+		for (int i = 1; i < promNotaAlumno.length; i++) {
+			
+			
+			if ( auxPromMasAlto <= promNotaAlumno[i] ) {
 				
-				if ( auxNotaMasBaja.equals(notaAlumno[i])) {
-					contadorNotaMasBaja++;
+				if ( auxPromMasAlto.equals(promNotaAlumno[i]) ) {
+					contadorPromNotaMasAlta++;
 				} else {
-					contadorNotaMasBaja = 1;
+					contadorPromNotaMasAlta = 1;
 				}
-				auxNotaMasBaja = notaAlumno[i];
+				auxPromMasAlto = promNotaAlumno[i];
+			}
+			
+			if (auxPromMasBajo >= promNotaAlumno[i]) {
+				
+				if ( auxPromMasBajo.equals(promNotaAlumno[i])) {
+					contadorPromNotaMasBaja++;
+				} else {
+					contadorPromNotaMasBaja = 1;
+				}
+				auxPromMasBajo = promNotaAlumno[i];
 			}
 			
 		}
@@ -123,16 +150,16 @@ public class Ejercicio {
 		int contadorPromocionan = 0;
 		int contadorRecursan = 0;
 		
-		for (int i = 0; i < notaAlumno.length; i++) {
-			if ( notaAlumno[i] >= 7) {
+		for (int i = 0; i < promNotaAlumno.length; i++) {
+			if ( promNotaAlumno[i] >= 7) {
 				contadorPromocionan++;
 			} else {
 				contadorRecursan++;
 			}
 		}
 		
-		alumnoNotaMasAlta = new Object[contadorNotaMasAlta][4];
-		alumnoNotaMasBaja = new Object[contadorNotaMasBaja][4];
+		alumnoNotaMasAlta = new Object[contadorPromNotaMasAlta][4];
+		alumnoNotaMasBaja = new Object[contadorPromNotaMasBaja][4];
 		alumnoPromocionan = new Object[contadorPromocionan][4];
 		alumnoRecursan = new Object[contadorRecursan][4];
 		
@@ -144,42 +171,44 @@ public class Ejercicio {
 
 		//carga de matrices, sabemos que auxNotaMasAlta & alumnoNotaMasBaja son notas mas alta y baja respectivamente
 		
-		for (int i = 0; i < notaAlumno.length; i++) {
+		for (int i = 0; i < promNotaAlumno.length; i++) {
 			
-			if ( auxNotaMasAlta.equals(notaAlumno[i]) ) {
+			if ( auxPromMasAlto.equals(promNotaAlumno[i]) ) {
 				
 				alumnoNotaMasAlta[posNotaMasAlta][0] = nombre[i];
 				alumnoNotaMasAlta[posNotaMasAlta][1] = apellido[i];
-				alumnoNotaMasAlta[posNotaMasAlta][2] = numeroDeExamen[i];
-				alumnoNotaMasAlta[posNotaMasAlta][3] = notaAlumno[i];
+				alumnoNotaMasAlta[posNotaMasAlta][2] = notaAlumno[i];
+				alumnoNotaMasAlta[posNotaMasAlta][3] = promNotaAlumno[i];
 				
 				posNotaMasAlta++;
 
 			}
 			
-			if ( auxNotaMasBaja.equals(notaAlumno[i]) ) {
+			if ( auxPromMasBajo.equals(promNotaAlumno[i]) ) {
 				
 				alumnoNotaMasBaja[posNotaMasBaja][0] = nombre[i];
 				alumnoNotaMasBaja[posNotaMasBaja][1] = apellido[i];
-				alumnoNotaMasBaja[posNotaMasBaja][2] = numeroDeExamen[i];
-				alumnoNotaMasBaja[posNotaMasBaja][3] = notaAlumno[i];
+				alumnoNotaMasBaja[posNotaMasBaja][2] = notaAlumno[i];
+				alumnoNotaMasBaja[posNotaMasBaja][3] = promNotaAlumno[i];
 				
 				posNotaMasBaja++;
 			}
 			
-			if ( notaAlumno[i] >= 7) {
+			if ( promNotaAlumno[i] >= 7) {
 				
 				alumnoPromocionan[posPromocionan][0] = nombre[i];
 				alumnoPromocionan[posPromocionan][1] = apellido[i];
-				alumnoPromocionan[posPromocionan][2] = numeroDeExamen[i];
-				alumnoPromocionan[posPromocionan][3] = notaAlumno[i];
+				alumnoPromocionan[posPromocionan][2] = notaAlumno[i];
+				alumnoPromocionan[posPromocionan][3] = promNotaAlumno[i];
+
 				posPromocionan++;
 				
 			} else {
 				alumnoRecursan[posRecursan][0] = nombre[i];
 				alumnoRecursan[posRecursan][1] = apellido[i];
-				alumnoRecursan[posRecursan][2] = numeroDeExamen[i];
-				alumnoRecursan[posRecursan][3] = notaAlumno[i];
+				alumnoRecursan[posRecursan][2] = notaAlumno[i];
+				alumnoRecursan[posRecursan][3] = promNotaAlumno[i];
+
 				posRecursan++;
 			}
 		}
@@ -191,39 +220,52 @@ public class Ejercicio {
 		System.out.println("****************************");
 		System.out.println("Resultados:");
 		
+		System.out.println();
 		System.out.println("1- alumno/s con la calificación mas alta");
 		for (int i = 0; i < alumnoNotaMasAlta.length; i++) {
 			
-			for (int j = 0; j < alumnoNotaMasAlta[i].length; j++) {
-				System.out.println(alumnoNotaMasAlta[i][j]);
-			}
+			System.out.println("Nombre: " + alumnoNotaMasAlta[i][0]);
+			System.out.println("Apellido: " + alumnoNotaMasAlta[i][1]);
+			System.out.println("Notas: " + Arrays.toString((Float[]) alumnoNotaMasAlta[i][2]));
+			System.out.println("Promedio: " + alumnoNotaMasAlta[i][3]);
+			
 			System.out.println("------------------------------");
 		}
 		
+		System.out.println();
 		System.out.println("****************************");
 		System.out.println("2- alumno/s con la calificación mas baja");
 		for (int i = 0; i < alumnoNotaMasBaja.length; i++) {
-			for (int j = 0; j < alumnoNotaMasBaja[i].length; j++) {
-				System.out.println(alumnoNotaMasBaja[i][j]);
-			}
+			
+			System.out.println("Nombre: " + alumnoNotaMasBaja[i][0]);
+			System.out.println("Apellido: " + alumnoNotaMasBaja[i][1]);
+			System.out.println("Notas: " + Arrays.toString((Float[]) alumnoNotaMasBaja[i][2]));
+			System.out.println("Promedio: " + alumnoNotaMasBaja[i][3]);
 			System.out.println("------------------------------");
 		}
 		
+		System.out.println();
 		System.out.println("****************************");
 		System.out.println("3- alumnos que promocionan, aquellos que obtubieron un promerdio >= 7");
 		for (int i = 0; i < alumnoPromocionan.length; i++) {
-			for (int j = 0; j < alumnoPromocionan[i].length; j++) {
-				System.out.println(alumnoPromocionan[i][j]);
-			}
+			
+			System.out.println("Nombre: " + alumnoPromocionan[i][0]);
+			System.out.println("Apellido: " + alumnoPromocionan[i][1]);
+			System.out.println("Notas: " + Arrays.toString((Float[]) alumnoPromocionan[i][2]));
+			System.out.println("Promedio: " + alumnoPromocionan[i][3]);
 			System.out.println("------------------------------");
 		}
 		
+		System.out.println();
 		System.out.println("****************************");
 		System.out.println("4- alumnos que deben recursar la materia, aquellos que obtuvieron un promedio < 7");
 		for (int i = 0; i < alumnoRecursan.length; i++) {
-			for (int j = 0; j < alumnoRecursan[i].length; j++) {
-				System.out.println(alumnoRecursan[i][j]);
-			}
+			
+			System.out.println("Nombre: " + alumnoRecursan[i][0]);
+			System.out.println("Apellido: " + alumnoRecursan[i][1]);
+			System.out.println("Notas: " + Arrays.toString((Float[]) alumnoRecursan[i][2]));
+			System.out.println("Promedio: " + alumnoRecursan[i][3]);
+			
 			System.out.println("------------------------------");
 		}
 		
